9540e9f14f951a33f3019969b9af99b7665c500c
avalz <avalenza89@gmail.com>
2017-02-05 15:40:53 +0100

    New jekyll

--- administrator/components/com_admintools/admintools.lastversion.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined('_JEXEC') or die();
-define('ADMINTOOLS_LASTVERSIONCHECK','2.6.2');
--- administrator/components/com_admintools/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Protect from unauthorized access
-defined('_JEXEC') or die('Restricted Access');
-
-define('ADMINTOOLS_VERSION', '2.6.2');
-define('ADMINTOOLS_DATE', '2014-03-10');
-define('ADMINTOOLS_PRO','1');
